f32cd283e3b99b226c5f77f086a4477025591f3a
Eleazar Ramos <e@fureyfs.com>
2020-07-10 16:14:52 -0400

    started baseline of app homepage

--- App.js
+++ App.js
@@ -1,21 +1,109 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useState, useEffect } from 'react'
+import {
+  StyleSheet,
+  Text,
+  Button,
+  SafeAreaView,
+  View,
+  FlatList,
+  ScrollView,
+  TouchableHighlight,
+  TextInput,
+} from 'react-native'
+import moment from 'moment'
+
+const FAKE_THOUGHTS = [
+  { id: '1', text: 'You should look at different things', date: new Date()},
+  { id: '2', text: 'What is my favorite thing to do?', date: new Date()},
+  { id: '3', text: 'Never shoot yourself in the foot you know', date: new Date()},
+]
+
+function Thought({ thought }) {
+  const [value, setValue] = useState('')
+
+  useEffect(() => {
+    thought.text && thought.text.length > 0 && setValue(thought.text)
+  }, [thought])
+
+  const dateText = moment(thought.date).fromNow()
+
+  return (
+    <View style={styles.thoughtContainer}>
+      <View style={styles.thoughtDateContainer}>
+        <Text style={styles.thoughtDateText}>{dateText}</Text>
+      </View>
+      <TextInput
+        style={styles.thoughtInput}
+        value={value}
+        onChangeText={(text) => setValue(text)}
+        multiline={true}
+      />
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.main}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Thoughts</Text>
+      </View>
+      <FlatList
+        contentContainerStyle={styles.list}
+        data={FAKE_THOUGHTS}
+        renderItem={({ item }) => <Thought thought={item} />}
+        keyExtractor={(item) => item.id}
+      />
       <StatusBar style="auto" />
-    </View>
-  );
+    </SafeAreaView>
+  )
+}
+
+const colors = {
+  orange: '#ff934f',
+  yellow: '#c2e812',
+  green: '#91f5ad',
+  white: '#ffffff',
+  grey: '#bfcbc2',
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.green,
   },
-});
+  header: {
+    padding: 10,
+  },
+  list: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
+    width: '100%',
+  },
+  title: {
+    fontSize: 36,
+    fontFamily: 'AvenirNext-Medium',
+  },
+  thoughtContainer: {
+    width: '100%',
+    marginBottom: 20,
+  },
+  thoughtDateContainer: {
+    width: '100%',
+    alignItems: 'flex-end',
+  },
+  thoughtDateText: {
+    color: colors.white,
+    backgroundColor: colors.orange,
+    paddingHorizontal: 5,
+    paddingHorizontal: 10,
+  },
+  thoughtInput: {
+    width: '100%',
+    padding: 10,
+    backgroundColor: colors.white,
+  },
+})
